9aea448252758c2671a1fc6ff16a650c14edf499
andreaselenaa <159645343+andreaselenaa@users.noreply.github.com>
2026-04-13 23:50:42 -0400

    Update util.js

--- util.js
+++ util.js
@@ -1,32 +1,30 @@
-// replace with your fork of the completed lab 3 repl https://replit.com/@Snickdx/INFO-2602-Lab-3-Completed
-const server = "https://9d12af7b-a3db-40f8-874b-72cc4ceba9dc-00-1mhomfu4s2k6l.riker.replit.dev";
+const server = "http://127.0.0.1:8000";
 
 function toast(message){
   M.toast({html: message});
 }
 
-async function sendRequest(url, method, data){
-  try{
-    //retrieve token from localStorage
+async function sendRequest(url, method, data) {
+  try {
     let token = window.localStorage.getItem('access_token');
 
-    let options = {//options passed to fetch function
-        method: method,
-        headers: { 
-          'Content-Type' : 'application/json',
-          'Authorization' : `Bearer ${token}`//send token in request
-        }
+    let options = {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token ? `Bearer ${token}` : ""
+      }
     };
 
-    if(data)//data will be given for PUT & POST requests
-      options.body = JSON.stringify(data);//convert data to JSON string
+    if (data) {
+      options.body = JSON.stringify(data);
+    }
 
-    let response = await fetch(url, options);
-      
-    let result = await response.json();//Get json data from response
-    return result;//return the result
+    let response = await fetch(server + url, options);
+    let result = await response.json();
+    return result;
 
-  }catch(error){
-    return {error: error};//catch and log any errors
+  } catch (error) {
+    return { error: error };
   }
 }
